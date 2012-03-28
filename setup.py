26f5e540764528d96e5a49c03e86fb95b4b8dfae
Drew Yeaton <xeeton@gmail.com>
2012-03-27 19:05:07 -0500

    incremented version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ CLASSIFIERS = [
 KEYWORDS = 'sorting sortable queryset pagination django'
 
 setup(name='django-sortable',
-    version='0.1.1',
+    version='0.1.2',
     description='Flexible sorting for Django applications',
     long_description=LONG_DESCRIPTION,
     author='Drew Yeaton',
